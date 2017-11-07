585306ceaa70be04ae46e55e978ad412a9504184
YJ <leeyingjie_95@hotmail.com>
2017-11-08 03:29:57 +0800

    fixed query script

--- demo-ec2-query.sh
+++ demo-ec2-query.sh
@@ -1,1 +1,1 @@
-./dodgynet --debug --server ec2-13-228-183-203.ap-southeast-1.compute.amazonaws.com query "L5- Socket Programming.pdf" --out "L5- Socket Programming.pdf"
+./dodgynet --debug --server ec2-13-228-183-203.ap-southeast-1.compute.amazonaws.com query "L5- Socket Programming.pdf" --out "L5- Socket Programming.pdf.dodgy"
